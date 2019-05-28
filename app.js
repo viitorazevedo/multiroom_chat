c22ea1a80d6b75485b5d8ef6faf768f0c62d700c
viitorazevedo <42189057+viitorazevedo@users.noreply.github.com>
2019-05-28 17:40:53 -0300

    instalação Web Socket.

--- app.js
+++ app.js
@@ -2,6 +2,20 @@
 var app = require('./config/server');
 
 /* parametrizar a porta de escuta */
-app.listen(80, function(){
+var server = app.listen(60, function(){
     console.log('Servidor Online');
 });
+
+var io = require('socket.io').listen(server);
+
+/* Criar a conexão por websocket */ 
+io.on('connection', function(socket){
+    console.log('Usuário conectou');
+
+
+    socket.on('disconnect', function(){
+        console.log('Usuário desconectou');
+
+    })
+});
+
